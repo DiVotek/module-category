e251cd991025861a70c48d3bd1774a76fb432988
maxboyko <maxboiko@divotek.com>
2024-08-27 10:56:52 +0300

    add view action

--- Admin/CategoryResource/Pages/EditCategory.php
+++ Admin/CategoryResource/Pages/EditCategory.php
@@ -14,6 +14,7 @@ class EditCategory extends EditRecord
     {
         return [
             Actions\DeleteAction::make(),
+            Actions\ViewAction::make()->url(fn($record) => $record->route())->openUrlInNewTab(true),
         ];
     }
 }
